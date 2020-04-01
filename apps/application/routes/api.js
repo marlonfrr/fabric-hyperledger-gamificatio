8984df280f5abee06f7300f3850de42aa9f5f313
marlonfrr <ma.forero11@uniandes.edu.co>
2020-04-01 00:53:41 -0500

    automated push

--- apps/application/routes/api.js
+++ apps/application/routes/api.js
@@ -4,7 +4,7 @@ const express = require('express');
 let router = express.Router();
 const apiController = require('../controllers/apiController');
 
-router.get('/', apiController.get);
+router.get('/v1/transaction/', apiController.get);
 router.post('/v1/transaction/create', apiController.post);
 
 module.exports = router;
