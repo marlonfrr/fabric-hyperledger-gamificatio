d64ba0112b0bbb86016a923da94ad1c52974715c
marlonfrr <ma.forero11@uniandes.edu.co>
2020-04-13 15:06:56 -0500

    automated push

--- apps/application/controllers/apiController.js
+++ apps/application/controllers/apiController.js
@@ -11,7 +11,7 @@ module.exports.auth = (req, res) => {
   //     req.body.transactionId
   //   );
   //   let response = JSON.parse(result.toString());
-  res.status(200).json({data:{names: "Gryn"}});
+  res.status(200).json({names: "Gryn"});
   // });
 };
 
