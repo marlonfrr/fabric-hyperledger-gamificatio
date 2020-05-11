0a148a9c534e80680c3fe04cd0f978c12a56bbf2
marlonfrr <ma.forero11@uniandes.edu.co>
2020-05-11 17:03:02 -0500

    automated push

--- smartcontracts/contract/index.js
+++ smartcontracts/contract/index.js
@@ -374,18 +374,10 @@ var ABstore = class {
 
     // Get the state from the ledger
     let Avalbytes = await stub.getState(A);
-    console.log("-------------")
-    console.log(Avalbytes+ "njknjknjk piees")
-    console.log(typeof(Avalbytes)+ "typeof")
-    console.log(Avalbytes=='', "true o jalse")
-    console.log("-------------")
-
-    if (!Avalbytes) {
+    if (!Avalbytes || Avalbytes=='') {
       jsonResp.error = "Failed to get state for " + A;
-      throw new Error(jsonResp);
+      throw new Error(Buffer.from(JSON.stringify(jsonResp)));
     }
-
-    console.log("jajej")
 
     jsonResp.name = A;
     jsonResp.amount = Avalbytes.toString();
