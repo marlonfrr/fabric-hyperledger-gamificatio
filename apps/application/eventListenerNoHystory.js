c59b93baace493d4b9996f471a9e22ef896b06c0
marlonfrr <ma.forero11@uniandes.edu.co>
2020-05-19 18:03:34 -0500

    automated push

--- apps/application/eventListenerNoHystory.js
+++ apps/application/eventListenerNoHystory.js
@@ -5,6 +5,17 @@ const request = require('request');
 const path = require('path');
 const configPath = path.resolve(__dirname, 'nextblockEL.txt');
 const fs = require('fs');
+const aws = require("aws-sdk");
+aws.config.loadFromPath(process.cwd() + "/aws-config.json");
+const sns = new aws.SNS();
+
+let paramsPublish = {
+  Message: "blablabalbalabla",
+  TopicArn: "arn:aws:sns:us-west-1:156625418537:gamificatio-test"
+};
+
+// arn:aws:sns:us-west-1:156625418537:gamificatio-test
+
 
 var options = {
   method: 'POST',
@@ -43,13 +54,18 @@ getGateway
         event = JSON.parse(event);
         // console.log(`type: ${event.type}`);
         options.body = event;
-        request(options, function (error, response, body) {
-          if (error) {
-            throw new Error(error);
-          }
+        // request(options, function (error, response, body) {
+        //   if (error) {
+        //     throw new Error(error);
+        //   }
 
-          console.log(body);
-        });
+        //   console.log(body);
+        // });
+        // AWS logic
+        sns.publish(paramsPublish, function(err, data) {
+          if (err) console.log(err); 
+          else console.log("::::::::> Valid ",data);
+      });
       }
     );
   })
